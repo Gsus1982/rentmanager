683a6bc0cc202bc3db5cdf1b4cc97de4e0d7cb0e
Jesús Martínez Nicolás <jjmn24@educastillalamancha.es>
2026-01-22 14:09:43 +0100

    Add CsrfViewMiddleware and fix middleware order

--- config/settings.py
+++ config/settings.py
@@ -51,12 +51,16 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'config.urls'
 
