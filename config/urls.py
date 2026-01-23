d35324c6242459e800b71357ec468fb2dd181ab3
Jesús Martínez Nicolás <jjmn24@educastillalamancha.es>
2026-01-23 11:11:31 +0100

    Remove CustomAdminLoginView completely from urls

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from app.views import CustomAdminLoginView
+
 
 def home(request):
     return HttpResponse('RentManager está online ✓')
 
+
 urlpatterns = [
     path('', home, name='home'),
-    path('admin/login/', CustomAdminLoginView.as_view(), name='admin_login'),
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
 ]
