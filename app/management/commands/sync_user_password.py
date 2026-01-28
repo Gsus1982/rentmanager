b4344269fb3dfdbd420afef1feb377286e0f57a4
Jesús Martínez Nicolás <jjmn24@educastillalamancha.es>
2026-01-28 12:02:54 +0100

    Update sync_user_password to use Gsus1982

--- app/management/commands/sync_user_password.py
+++ app/management/commands/sync_user_password.py
@@ -5,7 +5,15 @@ class Command(BaseCommand):
     help = 'Sync user password'
 
     def handle(self, *args, **options):
-        user = User.objects.get(username='Gsus82')
+        # Elimina usuario antiguo si existe
+        User.objects.filter(username='Gsus82').delete()
+        
+        # Crea o actualiza el nuevo usuario
+        user, created = User.objects.get_or_create(username='Gsus1982')
         user.set_password('ab12345678')
+        user.is_staff = True
+        user.is_superuser = True
         user.save()
-        self.stdout.write(self.style.SUCCESS('Password updated'))
+        
+        status = "created" if created else "updated"
+        self.stdout.write(self.style.SUCCESS(f'User {status}'))
